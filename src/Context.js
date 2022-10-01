07d6bd59a7dc78970e1bb704b4471a86ad3c6cdd
Julian <105754345+jprune@users.noreply.github.com>
2022-10-01 13:34:00 +0200

    fix context.js

--- src/Context.js
+++ src/Context.js
@@ -5,26 +5,6 @@ export const YelpContext = createContext(); // 1. create context
 // eslint-disable-next-line react/function-component-definition
 export default function Context({ children }) { // 2. create a function
   const axios = require('axios');
-  const [restaurantList, setRestaurantList] = useState({});
-  const [tags, setTags] = useState(['Fast Food', 'Vegan', 'Drinks', 'Romantic', 'Extravagant']);
-  const [categories, setCategories] = useState(['Italian', 'Asian', 'French', 'Mexican', 'Chinese']);
-  //   useEffect(() => {
-  //     const getRestaurantList = async () => {
-  //       try {
-  //         const response = await axios.get('https://mini-yelp-clone-backend.herokuapp.com/restaurants');
-  //         // console.log(response);
-  //         setRestaurantList(response);
-  //       } catch (error) {
-  //         // console.error(error);
-  //       }
-  //     };
-  //     getRestaurantList();import { createContext, useState, useEffect } from 'react';
-
-export const YelpContext = createContext(); // 1. create context
-
-// eslint-disable-next-line react/function-component-definition
-export default function Context({ children }) { // 2. create a function
-  const axios = require('axios');
   const [restaurantList, setRestaurantList] = useState([
     {
       _id: '6337fce518dc251c2a411b10',
@@ -78,15 +58,6 @@ export default function Context({ children }) { // 2. create a function
   //       }
   //     };
   //     getRestaurantList();
-  //   }, []);
-
-  return (
-    <YelpContext.Provider value={{ restaurantList, setRestaurantList, tags, setTags, categories, setCategories }}>
-      {children}
-    </YelpContext.Provider>
-  );
-}
-
   //   }, []);
 
   return (
